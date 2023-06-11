b080403ce111976365a13d386bbc7f927cf1b4b2
niksbanna <bannanicky0@gmail.com>
2023-06-11 13:38:48 +0530

    fixed some bugs in product routes

--- app/routes/productsRoute.js
+++ app/routes/productsRoute.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { create, createProductReview, deleteReview, destroy, getProductReviews, index, show, update } from "../controllers/productsController.js";
+import { create, createProductReview, deleteReview, destroy, getAdminProducts, getProductReviews, index, show, update } from "../controllers/productsController.js";
 import { authoriseRoles, isAuthenticatedUser } from "../../middlewares/auth.js";
 export const productRouter = Router();
 
 productRouter.get('/', index);
 productRouter.get('/:id', show);
 productRouter.get('/reviews', getProductReviews);
+productRouter.get('/admin/products', getAdminProducts);
 productRouter.delete('/reviews', isAuthenticatedUser, deleteReview);
 productRouter.post('/create', isAuthenticatedUser, create);
 productRouter.put('/review', isAuthenticatedUser, createProductReview);
