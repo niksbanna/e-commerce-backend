cea63ccc7459a9c89465d9b3f05d0246cd229483
niksbanna <bannanicky0@gmail.com>
2023-06-12 12:49:03 +0530

    updated orderModel and removed some required fields

--- app/models/orderModel.js
+++ app/models/orderModel.js
@@ -4,29 +4,23 @@ const orderSchema = new mongoose.Schema({
     shippingInfo: {
         address: {
             type: String,
-            required: true,
         },
         city: {
             type: String,
-            required: true,
         },
 
         state: {
             type: String,
-            required: true,
         },
 
         country: {
             type: String,
-            required: true,
         },
         pinCode: {
             type: Number,
-            required: true,
         },
         phoneNo: {
             type: Number,
-            required: true,
         },
     },
     orderItems: [
@@ -62,11 +56,10 @@ const orderSchema = new mongoose.Schema({
     paymentInfo: {
         id: {
             type: String,
-            required: true,
+
         },
         status: {
             type: String,
-            required: true,
         },
     },
     paidAt: {
